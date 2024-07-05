75fb3c5ae3626b309b559f0d610d4260ff0362bf
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-07-05 15:11:32 +0800

    [Change] Include trailing slash for start_url

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 async function updatePWAManifest() {
-    const start_url = '/yukihira'
+    const start_url = '/yukihira/'
     console.log(`${crayon.lightYellow('Changing manifest.json for production:')}`)
     console.log(`   start_url: ${crayon.lightGreen(start_url)}`)
     manifest.start_url = start_url
