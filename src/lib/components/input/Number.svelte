843879b43884e189932b2812ffcede99cb26c8c8
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2025-02-07 11:52:20 +0800

    [Change] Add onchange listener on Number component

--- src/lib/components/input/Number.svelte
+++ src/lib/components/input/Number.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
-    let { value = $bindable(0) }: { value?: number } = $props()
+    type NumberInputProps = {
+        value?: number
+        change?: (value: number) => void
+    }
+
+    let { value = $bindable(0), change }: NumberInputProps = $props()
 
     function increment() {
         value++
+        change?.(value)
     }
 
     function decrement() {
         if (value !== 0) {
             value--
+            change?.(value)
         }
     }
 </script>
@@ -18,7 +25,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minus w-4 h-4"><path d="M5 12h14"/></svg>
         <span class="sr-only">Decrement</span>
     </button>
-    <input bind:value type="number" class="number-input focus:isolate w-12 appearance-none bg-saddle-100 text-saddle-800 text-center outline-none ring-2 ring-transparent ring-offset-2 ring-offset-transparent focus:ring-offset-saddle-900 focus:ring-saddle-500 focus:border-saddle-500" min="0">
+    <input onchange={(event) => change?.(Number(event.currentTarget.value))} bind:value type="number" class="number-input focus:isolate w-12 appearance-none bg-saddle-100 text-saddle-800 text-center outline-none ring-2 ring-transparent ring-offset-2 ring-offset-transparent focus:ring-offset-saddle-900 focus:ring-saddle-500 focus:border-saddle-500" min="0">
     <button onclick={increment} type="button" class="number-button increment focus:isolate bg-saddle-100 text-saddle-800 outline-none hover:bg-saddle-300 ring-2 ring-transparent ring-offset-2 ring-offset-transparent focus:ring-offset-saddle-900 focus:ring-saddle-500 focus:border-saddle-500 rounded-r-md px-1">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus w-4 h-4"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
         <span class="sr-only">Increment</span>
