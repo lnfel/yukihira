4e2dfe31e775bcdb6cdd70411b4d78f47a26a7c4
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2025-02-07 12:00:02 +0800

    [Dependency] Init shadcn-svelte deps, css and tailwind config

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,9 @@
 import ShortUniqueId from "short-unique-id"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export const shortUniqueID = new ShortUniqueId()
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
