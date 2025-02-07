2caa001f8e67f275c496080d9675db946eea3e5c
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2025-02-07 12:02:01 +0800

    [Feature] Add shadcn-svelte table component

---
+++ src/lib/components/ui/table/table.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { HTMLTableAttributes } from "svelte/elements";
+	import type { WithElementRef } from "bits-ui";
+	import { cn } from "$lib/utils/index.js";
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	}: WithElementRef<HTMLTableAttributes> = $props();
+</script>
+
+<div class="max-h-96 w-full relative rounded-md border overflow-auto">
+	<table bind:this={ref} class={cn("w-full caption-bottom text-sm", className)} {...restProps}>
+		{@render children?.()}
+	</table>
+</div>
